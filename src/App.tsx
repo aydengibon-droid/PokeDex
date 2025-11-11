8af68d3fb372ca802a0a462505901c26f95126b5
aydengibon-droid <aydengibon@gmail.com>
2025-11-11 14:25:16 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useEffect } from "react"
+
+function App() {
+  useEffect(() =>{}, [])
+
+  return (
+    <>
+      <div className="">
+
+      </div>
+    </>
+  )
+}
+
+export default App
