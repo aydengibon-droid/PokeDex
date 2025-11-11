d018237b869b4143bdd012b989f79453282934b3
aydengibon-droid <aydengibon@gmail.com>
2025-11-11 15:32:37 -0400

    pokemon1

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 function App() {
-  useEffect(() =>{}, [])
+const [pokemon, setPokemon] = useState(null)
+
+  useEffect(() =>{
+    const randomInt = (min:number,) "https://pokeapi.co/api/v2/pokemon/390"
+    fetch(URL, {
+      headers: {
+        Accept: "application/json"
+      }
+    })
+    .then(response => response.json())
+    .then(data => setPokemon(data))
+  }, [])
 
   return (
     <>
-      <div className="">
-
-      </div>
+      <p className="text-4xl">
+        {pokemon?.name}
+        {pokemon?.abilities.map(skill => {
+        return <p 
+        })}
+      </p>
+    
     </>
   )
 }
